811348d6634702269ed9b97ad342ecc1c92ea91b
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2024-01-16 16:20:14 +0100

    Update 5_Suscripcion.py

--- Pages/5_Suscripcion.py
+++ Pages/5_Suscripcion.py
@@ -1,1 +1,25 @@
+import streamlit as st
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts.chat import ChatPromptTemplate
+import os
 
+
+api_key1 = st.secrets["OPENAI_API_KEY"]
+
+api_key2 = os.getenv("OPENAI_API_KEY")
+
+chat_model = ChatOpenAI(openai_api_key=api_key1)
+
+template = "You area helpful saler of monthly suscriptions to climate insurances for Novus Clima Company. You know that save lifes and assets by insurance is like a be superheros in real time. You are welcoming customers and trying to sale insurance monthly basic by 10USD por person, medium by 50USD for cupples and large by 100USD for families and homes"
+human_template = "{text}"
+
+chat_prompt = ChatPromptTemplate.from_messages([
+  ("system", template),
+  ("human", human_template),
+])
+
+messages = chat_prompt.format_messages(text="Hi I would like to know what do you do in Novus Clima")
+
+result = chat_model.predict_messages(messages)
+
+st.write(result.content)
