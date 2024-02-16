689f556e886d9360737e13887e7dfc9868a6a2e4
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2024-02-16 16:11:29 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -215,77 +215,77 @@ with st.expander("The Challenge"):
 
 
 
-
-st.header("The Solution: Lead Cities to build their own Novus Clima 🏆")
-st.subheader("Seattle's Example")
-
-with st.container(border=True):
-    st.markdown(f'<p class="btc_text">Historic PATTERNS</p>', unsafe_allow_html = True)
-    st.subheader('Seattle Patterns')
-    source = data.seattle_weather()
+with st.expander("The Solution"):
+    st.header("Lead Cities to build their own Novus Clima 🏆")
+    st.subheader("Seattle's Example")
     
-    scale = alt.Scale(
-        domain=["sun", "fog", "drizzle", "rain", "snow"],
-        range=["#e7ba52", "#a7a7a7", "#aec7e8", "#1f77b4", "#9467bd"],
-    )
-    color = alt.Color("weather:N", scale=scale)
-    
-    # We create two selections:
-    # - a brush that is active on the top panel
-    # - a multi-click that is active on the bottom panel
-    brush = alt.selection_interval(encodings=["x"])
-    click = alt.selection_multi(encodings=["color"])
-    
-    # Top panel is scatter plot of temperature vs time
-    points = (
-        alt.Chart()
-        .mark_point()
-        .encode(
-            alt.X("monthdate(date):T", title="Date"),
-            alt.Y(
-                "temp_max:Q",
-                title="Maximum Daily Temperature (C)",
-                scale=alt.Scale(domain=[-5, 40]),
-            ),
-            color=alt.condition(brush, color, alt.value("lightgray")),
-            size=alt.Size("precipitation:Q", scale=alt.Scale(range=[5, 200])),
+    with st.container(border=True):
+        st.markdown(f'<p class="btc_text">Historic PATTERNS</p>', unsafe_allow_html = True)
+        st.subheader('Seattle Patterns')
+        source = data.seattle_weather()
+        
+        scale = alt.Scale(
+            domain=["sun", "fog", "drizzle", "rain", "snow"],
+            range=["#e7ba52", "#a7a7a7", "#aec7e8", "#1f77b4", "#9467bd"],
         )
-        .properties(width=550, height=300)
-        .add_selection(brush)
-        .transform_filter(click)
-    )
-    
-    # Bottom panel is a bar chart of weather type
-    bars = (
-        alt.Chart()
-        .mark_bar()
-        .encode(
-            x="count()",
-            y="weather:N",
-            color=alt.condition(click, color, alt.value("lightgray")),
+        color = alt.Color("weather:N", scale=scale)
+        
+        # We create two selections:
+        # - a brush that is active on the top panel
+        # - a multi-click that is active on the bottom panel
+        brush = alt.selection_interval(encodings=["x"])
+        click = alt.selection_multi(encodings=["color"])
+        
+        # Top panel is scatter plot of temperature vs time
+        points = (
+            alt.Chart()
+            .mark_point()
+            .encode(
+                alt.X("monthdate(date):T", title="Date"),
+                alt.Y(
+                    "temp_max:Q",
+                    title="Maximum Daily Temperature (C)",
+                    scale=alt.Scale(domain=[-5, 40]),
+                ),
+                color=alt.condition(brush, color, alt.value("lightgray")),
+                size=alt.Size("precipitation:Q", scale=alt.Scale(range=[5, 200])),
+            )
+            .properties(width=550, height=300)
+            .add_selection(brush)
+            .transform_filter(click)
         )
-        .transform_filter(brush)
-        .properties(
-            width=550,
+        
+        # Bottom panel is a bar chart of weather type
+        bars = (
+            alt.Chart()
+            .mark_bar()
+            .encode(
+                x="count()",
+                y="weather:N",
+                color=alt.condition(click, color, alt.value("lightgray")),
+            )
+            .transform_filter(brush)
+            .properties(
+                width=550,
+            )
+            .add_selection(click)
         )
-        .add_selection(click)
-    )
+        
+        chart = alt.vconcat(points, bars, data=source, title="Historic Seattle Weather: 2012-2015")
+        st.altair_chart(chart, theme="streamlit", use_container_width=True)
     
-    chart = alt.vconcat(points, bars, data=source, title="Historic Seattle Weather: 2012-2015")
-    st.altair_chart(chart, theme="streamlit", use_container_width=True)
-
-with st.container(border=True):
-    st.markdown(f'<p class="btc_text">Monitoring TODAY</p>', unsafe_allow_html = True)
+    with st.container(border=True):
+        st.markdown(f'<p class="btc_text">Monitoring TODAY</p>', unsafe_allow_html = True)
+        
     
-
-
-
-with st.container(border=True):
-    st.markdown(f'<p class="btc_text">Alarming RIGH NOW</p>', unsafe_allow_html = True)
-    st.subheader('Risk Dissasters Predictions')
-
-
-with st.container(border=True):
-    st.markdown(f'<p class="btc_text">Preventing TOMORROW</p>', unsafe_allow_html = True)
-    st.subheader('Best Practices')
-
+    
+    
+    with st.container(border=True):
+        st.markdown(f'<p class="btc_text">Alarming RIGH NOW</p>', unsafe_allow_html = True)
+        st.subheader('Risk Dissasters Predictions')
+    
+    
+    with st.container(border=True):
+        st.markdown(f'<p class="btc_text">Preventing TOMORROW</p>', unsafe_allow_html = True)
+        st.subheader('Best Practices')
+    
